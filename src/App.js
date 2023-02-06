26f85d9e45ebab4d7d899d967f530bc0f428195f
Vincenzo M <vincenzomaiorana@gmail.com>
2023-02-06 13:09:52 +0100

    added some buttns and images in home

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import ButtonComponent from './components/ButtonComponent';
+import ImageComponent from './components/ImageComponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <ButtonComponent content="Tasto a caso creato con Funzione" />
+          <ButtonComponent content="Un altro Tasto" />
+          <ButtonComponent content="Un terzo Tasto" />
+        </div>
+        <div>
+          <ImageComponent src="https://placekitten.com/200" alt="Un bel gattino" />
+          <ImageComponent src="https://placedog.net/200/200" alt="Un bel cagnolino" />
+        </div>
       </header>
     </div>
   );
